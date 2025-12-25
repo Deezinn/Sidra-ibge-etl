4cd88917a75146f72c79492e71e8b9f5d3af74b2
Deezinn <developer.andrelz@gmail.com>
2025-12-25 09:37:35 -0300

    fix: fixed extract

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -4,38 +4,52 @@ from datetime import datetime
 import json
 import pandas as pd
 class Extract():
-    def __init__(self) -> None:
+    def __init__(self, api: dict) -> None:
+        self.__api = api
+    
+    def extract_quarter_api(self) -> dict:
+        yearBase = 2025 # menor valor 1997
+        quarter = 1
+        
+        now = datetime.now()
+        year_and_quarter = f'{now.year}0{((now.month - 1) // 3 + 1)}'
+        
+        content_serie = {}
+        content_temp = []
+    
+        for serie, api in self.__api.items():
+            year = yearBase
+            quarter = 1
+            
+            while True:
+                year_and_quarter_base = f'{year}0{quarter}'
+                
+                api_splitted = api.split('/')
+                index_to_replace = api_splitted.index('first%201')
+                
+                api_splitted[index_to_replace] = year_and_quarter_base
+                api_joined = '/'.join(api_splitted)
+                
+                r = requests.get(api_joined, timeout=30)
+                
+                if r.status_code == 200:
+                    content_temp.extend(r.json())
+                else:
+                    r.raise_for_status()
+                    break
+                
+                if year_and_quarter_base == year_and_quarter:
+                    break
+                
+                quarter += 1
+                
+                if quarter > 4:
+                    quarter =  1
+                    year += 1
+                
+            content_serie[serie] = content_temp
+        return content_serie    
+    
+    def extract_month_api(self):
         pass
     
-    def run(self):
-        quarter = 1
-        yearBase = 2024 # menor valor 1997
-        year_now, month_now = datetime.now().year, (datetime.now().month - 1) // 3 + 1
-        year_and_quarter_now = f'{year_now}0{month_now -1}'
-        flag = True
-        
-        content_api = {}
-        
-        while flag:
-            for serie,api in APISABATE.items():
-                
-                link_splited = api.split('/')
-                link_splited[13] = f'{yearBase}0{quarter}'
-                link_joined = '/'.join(link_splited)
-                
-                r = requests.get(link_joined, timeout=30)
-                
-                if r.status_code == 200 and link_splited[13] != year_and_quarter_now :
-                    if quarter > 3:
-                        quarter = 0
-                        yearBase += 1
-                    quarter += 1
-                else:
-                    flag = False
-                
-                content_api[f'abate_{serie}'] = r.json()
-
-        return content_api
-    
-e = Extract()   
-e.run()
