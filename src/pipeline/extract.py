48a27f24c7e5ecf3f914d7356102f01624a11687
Deezinn <developer.andrelz@gmail.com>
2026-01-04 07:32:32 -0300

    feat: extract temporary csv to transform dataset in ipynb

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -9,16 +9,16 @@ class Extract():
     def _link_is_valid(link: str, serie: str):
         """
         Verifica se o link está batendo com a série
-        
+
         :param link: recebe o link da constante
         :type link: str
         :param serie: recebe a serie da constante
         :type serie: str
         """
-        
+
         link_splitted:list = link.split('/')
         serie_splitted:list = serie.split('_')
-        
+
         for value in serie_splitted:
             if value.isdigit():
                 if value in link_splitted:
@@ -28,194 +28,193 @@ class Extract():
 
     def run_quarterly(self, link: str, serie: str) -> list:
         self._link_is_valid(link=link, serie=serie)
-        
+
         if not isinstance(link, str):
             raise TypeError('')
-        
+
         if not link:
             raise ValueError('')
-        
+
         data:list = []
-        
-        year_init:int = 2020
-        quarter_init:int = 1
-                
+
+        year_init:int = 2025
+        quarter_init:int = 3
+
         now:datetime = datetime.now()
-        
+
         current_quarter:int = (now.month - 1)// 3 + 1
         current_year:int = now.year
-                
+
         link_splitted:list = link.split('/')
-        
+
         if 'first%201' not in link_splitted:
-            raise ValueError(f'link não contém o placeholder "first%201" {serie}')  
-        
+            raise ValueError(f'link não contém o placeholder "first%201" {serie}')
+
         index_to_replace:int = link_splitted.index('first%201')
-        
+
         while (year_init,quarter_init) <= (current_year,current_quarter):
-            
+
             if quarter_init > 4:
                 quarter_init = 1
-                
+
             if quarter_init == 4:
                 year_init += 1
-            
+
             link_splitted[index_to_replace] = f'{year_init}{quarter_init:02d}'
             link_joined:str = '/'.join(link_splitted)
-            
+
             response = requests.get(link_joined)
             response.raise_for_status()
-            
+
             payload = response.json()
-            
+
             if not payload:
                 raise InvalidJSONError('')
-            
+
             data.extend(payload)
-                    
+
             quarter_init += 1
         return data
-                
+
     def run_monthly(self, link: str, serie: str) -> list:
         self._link_is_valid(link=link, serie=serie)
-        
+
         if not isinstance(link, str):
             raise TypeError('')
-        
+
         if not link:
             raise ValueError('')
-        
+
         data:list = []
-        
-        year_init:int = 2020
-        month_init:int = 1
-        
+
+        year_init:int = 2025
+        month_init:int = 11
+
         now:datetime = datetime.now()
         current_year:int = now.year
         current_month:int = now.month
-              
+
         link_splitted:list = link.split('/')
-        
+
         if 'first%201' not in link_splitted:
-            raise ValueError(f'link não contém o placeholder "first%201" {serie}')  
-        
+            raise ValueError(f'link não contém o placeholder "first%201" {serie}')
+
         index_to_replace:int = link_splitted.index('first%201')
-        
+
         while (year_init, month_init) <= (current_year, current_month):
-            
+
             if month_init > 12:
                 month_init = 1
                 year_init += 1
-            
+
             link_splitted[index_to_replace] = f'{year_init}{month_init:02d}'
             link_joined:str = '/'.join(link_splitted)
-            
+
             response = requests.get(link_joined)
             response.raise_for_status()
-            
+
             payload = response.json()
-            
+
             if not payload:
                 raise InvalidJSONError('')
-            
+
             data.extend(payload)
-            
+
             month_init += 1
         return data
 
     def run_semester(self, link: str, serie: str) -> list:
         self._link_is_valid(link=link, serie=serie)
-        
+
         if not isinstance(link, str):
             pass
-        
+
         if not link:
             ValueError('')
-        
+
         data:list = []
-        
-        year_init:int = 2020
-        semester_init:int = 1
-        
+
+        year_init:int = 2025
+        semester_init:int = 2
+
         now:datetime = datetime.now()
         current_semester:int = (now.month - 1) // 6 + 1
         current_year:int = now.year
 
-        link_splitted = link.split('/') 
-        
+        link_splitted = link.split('/')
+
         if 'first%201' not in link_splitted:
-            raise ValueError(f'link não contém o placeholder "first%201" {serie}')  
-            
+            raise ValueError(f'link não contém o placeholder "first%201" {serie}')
+
         index_to_replace = link_splitted.index('first%201')
-        
+
         while (year_init, semester_init) <= (current_year, current_semester):
-            
+
             if semester_init > 2:
                 semester_init = 1
-                year_init += 1 
-                
+                year_init += 1
+
             link_splitted[index_to_replace] = f'{year_init}{semester_init:02d}'
             link_joined = '/'.join(link_splitted)
-            
+
             response = requests.get(link_joined)
             response.raise_for_status()
-            
+
             payload = response.json()
-            
+
             if not payload:
                 raise InvalidJSONError('')
-            
+
             data.extend(payload)
-            
+
             semester_init += 1
         return data
-    
+
     def run_one_monthly(self, link, serie: str) -> list:
         self._link_is_valid(link=link, serie=serie)
-        
+
         if not isinstance(link, str):
             pass
-        
+
         if not link:
             ValueError('')
-        
+
         data:list = []
-        
+
         response = requests.get(link)
         response.raise_for_status()
-        
+
         payload = response.json()
-        
+
         if not payload:
             raise InvalidJSONError('')
-        
+
         data.extend(payload)
-        return data 
-    
+        return data
+
     def run_three_monthly(self, link, serie) -> list:
         self._link_is_valid(link=link, serie=serie)
-        
+
         if not isinstance(link, str):
             ValueError('')
-        
+
         if not link:
             ValueError('')
-        
+
         data:list = []
-        
+
         link_splitted:list = link.split('/')
         index_to_replace:int = link_splitted.index('first%201')
-        
+
         link_splitted[index_to_replace] = 'all'
         link_joined = '/'.join(link_splitted)
-        
+
         response = requests.get(link_joined)
         payload = response.json()
-            
+
         if not payload:
             raise InvalidJSONError('')
-   
+
         data.extend(payload)
-            
+
         return data
-    
